9a633c89634dda55d3500c3779d1a8a03dae1cb3
Djet78 <vacheslavuslistyj@gmail.com>
2018-12-17 21:30:00 +0200

    starter settings

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+const BASE_DIR = __DIR__;
+
+const DEBUG = True;
+
+const DB = [
+    'host' => 'localhost',
+    'dbname' => 'bwt',
+    'user' => 'root',
+    'password' => 'Akabos123456S',
+];
+?>
