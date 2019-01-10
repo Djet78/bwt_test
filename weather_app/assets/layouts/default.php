825f50b4f0e5a4d33f0a56b6aef9fd939b4de484
Djet78 <vacheslavuslistyj@gmail.com>
2019-01-10 12:33:43 +0200

    Default layout (in progress)

--- weather_app/assets/layouts/default.php
+++ weather_app/assets/layouts/default.php
@@ -1,9 +1,90 @@
 <!DOCTYPE html>
-<html>
+<html class="occupy-all gradient__rt_azure">
 <head>
+    <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
+
+    <style>
+        <?php require "weather_app/assets/main.css"; ?>
+    </style>  
+
 </head>
-<body>
-    <?php echo $content; ?>
+
+<body class="flex 
+             flex__main-page-def 
+             occupy-all">
+
+    <div class="block__main 
+                flex 
+                flex__main-block-def 
+                rotate_right-12deg
+                gradient__rb_light-green">
+     
+        <header class="block__header 
+                       flex 
+                       flex__header-block-def">
+            
+            <div class="logo">
+                <p>Logo Placeholder</p>
+            </div>
+            
+            <nav class="flex">
+                    <?php if ($_SESSION['user_group'] == 'autorized') {?>
+                        <a class="btn 
+                                  flex 
+                                  btn__flex-column 
+                                  btn__style_main-nav" 
+                           href="/logout">
+                            <p class="rotate_left-12deg">logout</p>
+                        </a>       
+                        
+                        <a class="btn 
+                                  flex 
+                                  btn__flex-column 
+                                  btn__style_main-nav" 
+                           href="/logout">
+                            <p class="rotate_left-12deg">logout2</p>
+                        </a>
+                        
+                    <?php } else { ?>
+                        <a class="btn 
+                                  flex 
+                                  btn__flex-column 
+                                  btn__style_main-nav" 
+                            href="/register">
+                            <p class="rotate_left-12deg">register</p>
+                        </a>
+                        
+                        <a class="btn 
+                                  flex 
+                                  btn__flex-column 
+                                  btn__style_main-nav" 
+                           href="/login">
+                            <p class="rotate_left-12deg">login</p>
+                        </a>
+                    <?php } ?>
+            </nav>
+
+        </header>
+        <?php echo $content; ?>
+        <footer class="block__footer
+                       flex
+                       flex__footer-block-def">
+            <div class="author">
+            </div>
+            <div class="contacts">
+            </div>
+            <div class="copyright">
+            </div>
+            
+        </footer>
+       
+    </div>
+
+    <aside class="block__feedback 
+                  gradient__lb_light-green 
+                  rotate_left-12deg">
+    </aside>
+
 </body>
 </html>
--- weather_app/views/user/login.php
+++ weather_app/views/user/login.php
@@ -1,12 +1,28 @@
-<h2>I am login page</h2>
-
-<form method="POST">      
-    <p>Email*</p>
+<form class="flex__main-item" method="POST">      
+    <fieldset>
+        <legend>Log In</legend>
         <?php $handler->displayErrorsIfErrors('email'); ?>
-        <input type="email" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" required> 
+        <p>
+            <label for="email">Email*</label>
+            <input type="email" 
+                   name="email" 
+                   id="email" 
+                   value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" 
+                   maxlength="50"
+                   placeholder="example@domain.com"
+                   required>
+        </p>
         
-    <p>Password*</p>
-        <input type="password" name="password" required>
-    <p></p>
-        <input type="submit" value="Submit">
+
+        <p>
+            <label for="password">Password*</label>
+            <input type="password" 
+                   name="password" 
+                   id="password" 
+                   maxlength="128"
+                   required>
+        </p>
+
+        <p><input type="submit" value="Submit"></p>
+    </fieldset>
 </form>
